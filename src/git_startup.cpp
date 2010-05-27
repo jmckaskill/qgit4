48e130356f25cbff411269fc2b5c9b6565e234f0
Sebastian Thiel <byronimo@gmail.com>
2010-05-27 19:05:44 +0200

    Adjusted data retrieval not to be topologically ordered to make debugging of events easier as they happen in a certain timely order. Unfortunately the result gets worse the closer you get to the first commit as it draws lanes too far. The algorithm needs adjustments to deal with it

--- src/git_startup.cpp
+++ src/git_startup.cpp
@@ -491,7 +491,7 @@ bool Git::startParseProc(SCList initCmd, FileHistory* fh, SCRef buf) {
 
 bool Git::startRevList(SCList args, FileHistory* fh) {
 
-	QString baseCmd("git log --topo-order --no-color "
+    QString baseCmd("git log --no-color "
 
 #ifndef Q_OS_WIN32
 	                "--log-size " // FIXME broken on Windows
